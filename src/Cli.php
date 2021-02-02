066fefa7172dab2f57b6622a75beac8176ea304c
root <root@DESKTOP-V6788T7.localdomain>
2021-02-03 02:22:01 +0300

    php-cli tools update

---
+++ src/Cli.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Brain\Games\Cli
+
+function line( $msg = '' ) {
+	Streams::_call( 'line', func_get_args() );
+};
+
+function prompt( $question, $default = false, $marker = ': ', $hide = false ) {
+	return Streams::prompt( $question, $default, $marker, $hide );
+}
