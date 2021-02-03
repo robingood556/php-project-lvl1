8de3f4038506e0b6066c6727c0d99c8c5054ff45
root <root@DESKTOP-V6788T7.localdomain>
2021-02-03 14:17:46 +0300

    Update fail linter

--- src/Cli.php
+++ src/Cli.php
@@ -2,10 +2,12 @@
 
 namespace Brain\Games\Cli
 
-function line( $msg = '' ) {
-	Streams::_call( 'line', func_get_args() );
-};
+function line($msg = '')
+{
+    Streams::_call('line', func_get_args());
+}
 
-function prompt( $question, $default = false, $marker = ': ', $hide = false ) {
-	return Streams::prompt( $question, $default, $marker, $hide );
+function prompt($question, $default = false, $marker = ': ', $hide = false)
+{
+    return Streams::prompt($question, $default, $marker, $hide);
 }
